01f055356c921e1dc743ae7d40d0cbb0773aeed8
Edgar <edgarbagagem@gmail.com>
2025-12-08 19:36:00 +0000

    test: refactor TestHello to use subtests for better organization

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,20 +2,22 @@ package main
 
 import "testing"
 
-func TestHelloWorld(t *testing.T) {
-	got := HelloWorld()
-	target := "Hello, world!"
-
-	if got != target {
-		t.Errorf("got %q want %q", got, target)
-	}
-}
-
 func TestHello(t *testing.T) {
-	got := Hello("Leonardo Da Vinci")
-	target := "Hello, Leonardo Da Vinci!"
+	t.Run("saying hello to Leonardo", func(t *testing.T) {
+		got := Hello("Leonardo Da Vinci")
+		target := "Hello, Leonardo Da Vinci!"
 
-	if got != target {
-		t.Errorf("got %q want %q", got, target)
-	}
+		if got != target {
+			t.Errorf("got %q want %q", got, target)
+		}
+	})
+
+	t.Run("saying hello to the world", func(t *testing.T) {
+		got := Hello("")
+		target := "Hello, world!"
+
+		if got != target {
+			t.Errorf("got %q want %q", got, target)
+		}
+	})
 }
