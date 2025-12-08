517a901825bfd7f0252a6c38992969f5b824eb2d
Edgar <edgarbagagem@gmail.com>
2025-12-08 20:32:09 +0000

    test: enhance TestHello with additional language cases

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,17 +4,39 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to Leonardo", func(t *testing.T) {
-		got := Hello("Leonardo Da Vinci")
+		got := Hello("Leonardo Da Vinci", "")
 		want := "Hello, Leonardo Da Vinci!"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying hello to the world", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world!"
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Juan", "Spanish")
+		want := "Hola, Juan!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Bonnie", "French")
+		want := "Bonjour, Bonnie!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in German", func(t *testing.T) {
+		got := Hello("Hans", "German")
+		want := "Hallo, Hans!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
